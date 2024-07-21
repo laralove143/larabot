7cd870c256f0ba41bd00c21a84d9b81704e8fcb0
Lara Kayaalp <me@lara.lv>
2024-07-21 03:09:10 +0300

    add sentry support

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,53 +1,62 @@
-use tracing::warn;
+use std::env;
+
+use sentry::{release_name, ClientInitGuard, SessionMode};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
-pub fn init() -> anyhow::Result<()> {
+pub fn init() -> anyhow::Result<ClientInitGuard> {
+    let sentry_guard = sentry::init(sentry::ClientOptions {
+        auto_session_tracking: true,
+        dsn: Some(env::var("SENTRY_DSN")?.parse()?),
+        release: release_name!(),
+        session_mode: SessionMode::Request,
+        ..Default::default()
+    });
+
     let fmt_tracing_layer = tracing_subscriber::fmt::layer().without_time().pretty();
     let tracing_registry = tracing_subscriber::registry()
         .with(fmt_tracing_layer)
+        .with(sentry::integrations::tracing::layer())
         .with(EnvFilter::try_from_default_env()?);
 
-    if let Ok(layer) = tracing_journald::layer() {
-        tracing_registry.with(layer).try_init()?;
-    } else {
-        warn!("Not using journald for tracing.");
-        tracing_registry.try_init()?;
-    }
+    tracing_registry.try_init()?;
 
-    Ok(())
+    Ok(sentry_guard)
 }
 
 #[cfg(test)]
 mod tests {
     use std::env;
 
-    use anyhow::Result;
+    use anyhow::{anyhow, Result};
+    use dotenvy::dotenv;
+    use sentry::ClientInitGuard;
     use tracing::{debug, error, info, trace, warn};
 
-    fn init() -> Result<()> {
-        env::set_var("RUST_LOG", "trace");
-        crate::tracing::init()?;
+    fn init() -> Result<ClientInitGuard> {
+        dotenv()?;
+        env::set_var("RUST_LOG", "info");
 
-        Ok(())
+        crate::tracing::init()
     }
 
     #[test]
     fn log_message() -> Result<()> {
-        init()?;
+        let _sentry_guard = init()?;
 
         let value = 1_u8;
+        let err = anyhow!("error");
         trace!(value, "trace");
         debug!(value, "debug");
         info!(value, "info");
         warn!(value, "warn");
-        error!(value, "error");
+        error!(error = &*err, value, "error");
 
         Ok(())
     }
 
     #[test]
     fn log_compatibility() -> Result<()> {
-        init()?;
+        let _sentry_guard = init()?;
 
         log::info!("Message logged via log crate.");
 
